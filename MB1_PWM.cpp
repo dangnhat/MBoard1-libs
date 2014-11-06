1cc93ca0642399933db9ab2c98da9ea8d975b22f
nvhien1992 <nvhien1992@gmail.com>
2014-11-06 10:50:38 +0700

    pwm: repair code merged

--- MB1_PWM.cpp
+++ MB1_PWM.cpp
@@ -21,7 +21,6 @@ pwm_hw::pwm_hw(void)
     this->channel = 1;
     this->mode = TIM_OCMode_PWM1;
     this->period = 665;
-    this->duty_cycle = 50;
 }
 
 bool pwm_hw::pwm_init(pwm_params_t *pwm_params)
@@ -31,7 +30,6 @@ bool pwm_hw::pwm_init(pwm_params_t *pwm_params)
 
     /* save internal value */
     this->channel = pwm_params->pwm_channel;
-    this->duty_cycle = pwm_params->duty_cycle;
     this->period = pwm_params->period;
     this->mode = pwm_params->pwm_mode;
     this->timer = (uint8_t) pwm_params->timer;
@@ -72,7 +70,7 @@ bool pwm_hw::pwm_init(pwm_params_t *pwm_params)
     timer_base_init();
 
     /* PWMx mode configuration: Channel y */
-    output_channel_init();
+    output_channel_init(0);
 
     TIM_ARRPreloadConfig(timer_x[timer], ENABLE);
 
@@ -87,15 +85,17 @@ void pwm_hw::pwm_period_setup(uint16_t period)
     this->period = period; //update period.
 
     timer_base_init();
-
-    pwm_duty_cycle_setup(this->duty_cycle);
 }
 
-void pwm_hw::pwm_duty_cycle_setup(uint8_t duty_cycle)
+void pwm_hw::pwm_prescaler_setup(uint16_t prescaler)
 {
-    this->duty_cycle = duty_cycle; //update duty cycle.
+    this->prescaler = prescaler;
+    timer_base_init();
+}
 
-    output_channel_init();
+void pwm_hw::pwm_pulse_width_setup(uint16_t pulse_width)
+{
+    output_channel_init(pulse_width);
 }
 
 void pwm_hw::timer_base_init(void)
@@ -103,36 +103,54 @@ void pwm_hw::timer_base_init(void)
     TIM_TimeBaseInitTypeDef time_base_init_struct;
 
     time_base_init_struct.TIM_Period = this->period;
-    time_base_init_struct.TIM_Prescaler = prescaler;
+    time_base_init_struct.TIM_Prescaler = this->prescaler;
     time_base_init_struct.TIM_ClockDivision = 0;
     time_base_init_struct.TIM_CounterMode = TIM_CounterMode_Up;
 
     TIM_TimeBaseInit(timer_x[timer], &time_base_init_struct);
 }
 
-void pwm_hw::output_channel_init(void)
+void pwm_hw::output_channel_init(uint16_t timer_pulse)
 {
     TIM_OCInitTypeDef oc_init_struct;
 
     oc_init_struct.TIM_OCMode = this->mode;
-    oc_init_struct.TIM_Pulse = timer_pulse_convert(this->duty_cycle);
+    oc_init_struct.TIM_Pulse = timer_pulse;
     oc_init_struct.TIM_OutputState = TIM_OutputState_Enable;
     oc_init_struct.TIM_OCPolarity = TIM_OCPolarity_High;
     switch (this->channel) {
     case 1:
         TIM_OC1Init(timer_x[timer], &oc_init_struct);
-        TIM_OC1PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
         break;
     case 2:
         TIM_OC2Init(timer_x[timer], &oc_init_struct);
-        TIM_OC2PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
         break;
     case 3:
         TIM_OC3Init(timer_x[timer], &oc_init_struct);
-        TIM_OC3PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
         break;
     case 4:
         TIM_OC4Init(timer_x[timer], &oc_init_struct);
+        break;
+    default:
+        break;
+    }
+
+    pwm_oc_enable();
+}
+
+void pwm_hw::pwm_oc_enable(void)
+{
+    switch (this->channel) {
+    case 1:
+        TIM_OC1PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
+        break;
+    case 2:
+        TIM_OC2PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
+        break;
+    case 3:
+        TIM_OC3PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
+        break;
+    case 4:
         TIM_OC4PreloadConfig(timer_x[timer], TIM_OCPreload_Enable);
         break;
     default:
@@ -140,17 +158,22 @@ void pwm_hw::output_channel_init(void)
     }
 }
 
-uint16_t pwm_hw::timer_pulse_convert(uint8_t duty_cycle)
+void pwm_hw::pwm_oc_disable(void)
 {
-    uint16_t ccr_value;
-    uint8_t mod;
-
-    ccr_value = (duty_cycle * (this->period)) / 100;
-    mod = (duty_cycle * (this->period)) % 100;
-
-    if (mod >= 50) {
-        ccr_value++;
+    switch (this->channel) {
+    case 1:
+        TIM_OC1PreloadConfig(timer_x[timer], TIM_OCPreload_Disable);
+        break;
+    case 2:
+        TIM_OC2PreloadConfig(timer_x[timer], TIM_OCPreload_Disable);
+        break;
+    case 3:
+        TIM_OC3PreloadConfig(timer_x[timer], TIM_OCPreload_Disable);
+        break;
+    case 4:
+        TIM_OC4PreloadConfig(timer_x[timer], TIM_OCPreload_Disable);
+        break;
+    default:
+        break;
     }
-
-    return ccr_value;
 }
--- MB1_PWM.h
+++ MB1_PWM.h
@@ -25,14 +25,15 @@ typedef enum {
     gp_timer3 = 3,  //general-purpose timer
     gp_timer4 = 4,  //general-purpose timer
     gp_timer5 = 5   //general-purpose timer
-} timer_t;
+} pwm_timer_t;
 
 typedef struct {
-    timer_t timer;
+    pwm_timer_t timer;
     uint16_t pwm_mode;
     uint8_t pwm_channel;
     uint16_t period;
-    uint8_t duty_cycle; //0-100%
+    uint16_t prescaler;
+    uint16_t pulse_width;
 } pwm_params_t;
 }
 
@@ -56,34 +57,26 @@ public:
      */
     void pwm_period_setup(uint16_t period);
 
-    /**
-     * @brief Set up duty cycle of PWM.
-     *
-     * @param[in] duty_cycle
-     */
-    void pwm_duty_cycle_setup(uint8_t duty_cycle);
+    void pwm_prescaler_setup(uint16_t prescaler);
+
+    void pwm_pulse_width_setup(uint16_t pulse_width);
+
+    void pwm_oc_enable(void);
+    void pwm_oc_disable(void);
+protected:
+    uint16_t period;
+    uint16_t prescaler;
 private:
     uint8_t timer;
     uint8_t channel;
-    uint16_t period;
     uint16_t mode;
-    uint8_t duty_cycle;
 
     /**
      * @brief Initialize pwm base on timer.
      */
     void timer_base_init(void);
 
-    void output_channel_init(void);
-
-    /**
-     * @brief Convert the given duty_cycle to CCR register value.
-     *
-     * @param[in] duty_cycle Duty cycle value (0-100%).
-     *
-     * @return Value of CCR register.
-     */
-    uint16_t timer_pulse_convert(uint8_t duty_cycle);
+    void output_channel_init(uint16_t timer_pulse);
 };
 
 #endif //__MB1_PWM_H_
